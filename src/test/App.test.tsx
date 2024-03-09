3a0152eef8286b474f3527625c69e2e8346cf886
HarleyCo <gerson_harley@hotmail.com>
2024-03-09 09:08:55 -0500

    requested changes PR

--- src/test/App.test.tsx
+++ src/test/App.test.tsx
@@ -1,13 +1,13 @@
 // src/__ tests __/App.test.tsx
-import { render } from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
 import App from "../App"
 import '@testing-library/jest-dom'
 
-test('demo', () => {
-    expect(true).toBe(true)
-})
+describe ("<App />", () => {
+    test ("Validate contains the word", () => {
+        render(<App />);
 
-test("Renders the main page", () => {
-    render(<App />)
-    expect(true).toBeTruthy()
-})
+        const word = screen.getByText("Recette");
+        expect(word).toBeInTheDocument;
+    });
+});
